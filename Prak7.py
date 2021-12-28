ee4f8fc615bdc18fdad362da64b5f39b28b5205f
Abdullah Hudzafah <96789420+abdulbedul13@users.noreply.github.com>
2021-12-29 01:58:27 +0700

    Create Prak7.py

---
+++ Prak7.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 28 19:57:12 2021
+@author: Abdullah
+"""
+
+# ELEMEN KOMPETENSI 1
+def faktorial(angka):
+    faktor = 1
+    for i in range(1, angka + 1):
+        faktor *= i
+    return faktor
+
+input = int(input("PROGRAM MENCARI NILAI FAKTORIAL DARI SEBUAH ANGKA\nMasukkan angka: "))
+print("Nilai faktorialnya adalah:", faktorial(input))
